b88befecba42b84e4cea2189a0a3ccbfcdab7402
mcimam <imam123.ci@gmail.com>
2024-05-14 10:02:39 +0700

    change module name

--- payment_midtrans_bobby/__manifest__.py
+++ payment_midtrans_bobby/__manifest__.py
@@ -8,8 +8,8 @@
     "description": "Unofficial module for Midtrans payment gateway.",
     "depends": ["payment"],
     "external_dependencies": {"python": ["midtransclient"]},
-    'post_init_hook': 'post_init_hook',
-    'uninstall_hook': 'uninstall_hook',
+    "post_init_hook": "post_init_hook",
+    "uninstall_hook": "uninstall_hook",
     "data": [
         "views/payment_views.xml",
         "views/payment_midtrans_templates.xml",
@@ -18,7 +18,7 @@
     ],
     "assets": {
         "web.assets_frontend": [
-            "payment_midtrans/static/src/js/main.js",
+            "payment_midtrans_bobby/static/src/js/main.js",
         ],
     },
     "installable": True,
